34dc90a15765c7b6021b6ba500ed030164e8b9c3
Vasco Almeida <vasco.amorim_11@hotmail.com>
2025-11-21 15:59:31 +0000

    grid changes

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -25,12 +25,16 @@ export default function PokemonGrid({
 
   if (!filtered.length) {
     return (
-      <p className="opacity-70 italic mt-6">No Pokémon match these filters.</p>
+      <div className="w-max">
+        <p className="opacity-70 italic mt-6">
+          No Pokémon match these filters.
+        </p>
+      </div>
     );
   }
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+    <div className="grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10 gap-4">
       {filtered.map((pk) => (
         <PokemonCard
           key={pk.name}
